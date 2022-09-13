1fb24de9d28fbbc6ecc7fa8c8d9d34e33f2082c8
devleandrocoelho <devleandrocoelho@gmail.com>
2022-09-13 16:26:57 -0300

    primeira vez com variavel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!, Sophie");
+        String nome = "Sophie";
+        System.out.println("Hello world! " + nome);
     }
 }
